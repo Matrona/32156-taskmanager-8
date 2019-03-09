2941e9cd740f58fcdc8c011f6ebb725c23a175e0
Elizaveta <matronka@mail.ru>
2019-03-10 00:20:31 +0300

    Add class Component

---
+++ src/component.js
@@ -0,0 +1,34 @@
+import {createElement} from './utils.js';
+
+export class Component {
+  constructor() {
+    if (new.target === Component) {
+      throw new Error(`Can't instantiate Component, only concrete one.`);
+    }
+
+    this._element = null;
+  }
+
+  get element() {
+    return this._element;
+  }
+
+  get template() {
+    throw new Error(`You have to define template.`);
+  }
+
+  render() {
+    this._element = createElement(this.template);
+    this.bind();
+    return this._element;
+  }
+
+  unrender() {
+    this.unbind();
+    this._element = null;
+  }
+
+  bind() {}
+
+  unbind() {}
+}
